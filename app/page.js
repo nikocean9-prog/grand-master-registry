dd3a1ff8527b160134ee0425a27cd8cfd128ba7f
nikocean9-prog <nikocean9@gmail.com>
2026-09-03 22:30:39 +0930

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,23 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Grand Master Registry</h1>
+
+      <p>
+        Track the Yu-Gi-Oh! Magnificent Monsters Grand Master Rares
+        that have been pulled around the world.
+      </p>
+
+      <h2>0 / 3,600 Confirmed</h2>
+      <p>0% of the worldwide Grand Master print run documented</p>
+
+      <button>Submit a Pull</button>
+
+      <hr />
+
+      <h2>Magnificent Monsters</h2>
+
+      <p>The 18 Grand Master Rares will appear here.</p>
+    </main>
+  );
+}
